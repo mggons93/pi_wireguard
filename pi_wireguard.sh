1a69cb69472deae43d8e4a32fbb9b34ad186d30f
mggons93 <malagonsxp@hotmail.com>
2024-11-16 21:00:36 -0500

    Update pi_wireguard.sh

--- pi_wireguard.sh
+++ pi_wireguard.sh
@@ -1,23 +1,21 @@
 # Actualiza los índices de los paquetes
 sudo apt update && sudo apt upgrade -y
-
 # Instala Curl
 sudo apt install curl -y
-
 # Instala Git
 sudo apt install git -y
-
 # Añadir el repositorio oficial de Docker (si no lo tienes)
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo tee /etc/apt/keyrings/docker.asc
 echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
 # Actualizar la lista de paquetes después de añadir el repositorio de Docker
 sudo apt update
-
 # Instalar Docker y sus componentes
 sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-
+#Detener el servicio DNS
+sudo systemctl stop systemd-resolved
+sudo systemctl disable systemd-resolved
+#Creando directorios
 sudo mkdir /home/pihole
 sudo mkdir /home/wireguard
 cd
