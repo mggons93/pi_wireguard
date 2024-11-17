856f2f36bcaedfa191c6411f59c70c699c7a2ec4
mggons93 <malagonsxp@hotmail.com>
2024-11-16 19:35:18 -0500

    Update pi_wireguard.sh

--- pi_wireguard.sh
+++ pi_wireguard.sh
@@ -1,6 +1,7 @@
-sudo apt update && sudo apt upgrade && sudo apt install git -y
-
+sudo apt update && sudo apt upgrade && sudo apt install git && sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 mkdir /home/pihole
 mkdir /home/wireguard
-
-sudo git clone
+mkdir /home/pi-wireguard
+cd /home/pi-wireguard
+sudo git clone https://raw.githubusercontent.com/mggons93/pi_wireguard/refs/heads/main/docker-compose.yml
+sudo docker compose up -d
