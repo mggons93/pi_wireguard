a919cb4297f682ffc3e4d8cc4a6fbfdd7479bf26
mggons93 <malagonsxp@hotmail.com>
2024-11-18 21:53:38 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
-sudo su
-# Definir la ruta base usando $SUDO_USER para obtener el usuario original
-BASE_PATH="/home/$SUDO_USER/pi_wireguard"
+
+# Definir la ruta base usando $HOME o $SUDO_USER
+BASE_PATH="$HOME/pi_wireguard"
+
+# Verificar si el directorio y los archivos existen
+if [ ! -d "$BASE_PATH" ]; then
+    echo "Error: El directorio $BASE_PATH no existe."
+    exit 1
+fi
+
+if [ ! -f "$BASE_PATH/checkSO.sh" ]; then
+    echo "Error: El archivo $BASE_PATH/checkSO.sh no existe."
+    exit 1
+fi
+
+if [ ! -f "$BASE_PATH/solveDNS.sh" ]; then
+    echo "Error: El archivo $BASE_PATH/solveDNS.sh no existe."
+    exit 1
+fi
+
+if [ ! -f "$BASE_PATH/pi_wireguard.sh" ]; then
+    echo "Error: El archivo $BASE_PATH/pi_wireguard.sh no existe."
+    exit 1
+fi
 
 # Ejecutar los scripts
 bash $BASE_PATH/checkSO.sh
