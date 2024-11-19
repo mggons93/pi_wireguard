789f6d98b861b5207924194fbebccacd096df5d2
mggons93 <malagonsxp@hotmail.com>
2024-11-18 20:51:00 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ BASE_PATH="/home/$USER/pi_wireguard"
 bash $BASE_PATH/checkSO.sh
 sleep 5
 
-# bash $BASE_PATH/solveDNS.sh
-# sleep 5
+bash $BASE_PATH/solveDNS.sh
+sleep 5
 
 bash $BASE_PATH/pi_wireguard.sh
